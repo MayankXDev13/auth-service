3ce2e2ad9816a3dda208182cad87c9a368f89bb4
Mayank <vijaymaynk2003@gmail.com>
2025-12-26 14:25:39 +0530

    improve

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,14 +6,9 @@ import { env } from "./env";
 // Database connection pool
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
-  max: 20, // Maximum number of connections
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
   ssl: {
     rejectUnauthorized: false
-  },
-  // Additional Neon PostgreSQL settings
-  application_name: "auth-service"
+  }
 });
 
 const db = drizzle(pool, { schema: Schema });

1e22f7df251b72a2f08163418600236d0b1509ce
Mayank <vijaymaynk2003@gmail.com>
2025-12-26 11:21:56 +0530

    remove the frontednt folder and move to another repo

--- auth-frontend/src/lib/oauth.ts
+++
@@ -1,9 +0,0 @@
-const API_URL = "http://localhost:4000/api/v1";
-
-export const loginWithGoogle = () => {
-  window.location.href = `${API_URL}/users/google`;
-};
-
-export const loginWithGithub = () => {
-  window.location.href = `${API_URL}/users/github`;
-};
